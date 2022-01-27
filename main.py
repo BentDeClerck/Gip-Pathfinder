b35ecec23b6b5350665a72d63f1fca1125112cd8
Satou <79169964+Satou-afk@users.noreply.github.com>
2022-01-27 16:13:41 +0100

    Grid begin

--- main.py
+++ main.py
@@ -9,18 +9,38 @@ from pygame.draw import rect
 Row = 10
 Colom = 10
 
+Window_height=1080
+Window_width=1920
+
+BlockSize = 10
+
+#Colors#
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
 # Setup #
 pg.init()
 
-Window_height=1080
-Window_width=1920
 Window = pg.display.set_mode((Window_width,Window_height))
+Window.fill (WHITE)
 pg.display.set_caption("Pathfinding")
 
 clock = pg.time.Clock()
 clock.tick(60)
 
-grid = [[0 for x in range(Row)] for y in range(Colom)]
+# Grid Create#
+def Makegrid(Row, Colom):
+	grid = []
+	gap = Colom // rows
+	for i in range(Row):
+		grid.append([])
+		for j in range(Row):
+			spot = Spot(i, j, gap, Row)
+			grid[i].append(spot)
+
+	return grid
 
 # run loop #
 run=True
