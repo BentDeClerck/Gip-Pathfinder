af55e89bb4bd0ca8920d27d732109b484d2f72d8
Satou <79169964+Satou-afk@users.noreply.github.com>
2022-02-17 15:52:02 +0100

    Grid

--- main.py
+++ main.py
@@ -24,10 +24,15 @@ pg.init()
 
 Window_height =  Colom * BlockSize
 Window_width = Row * BlockSize
+Colored_Block = BlockSize - 1
 
-Window = pg.display.set_mode((Window_width,Window_height))
+Window = pg.display.set_mode((Window_width, Window_height))
 Window.fill (WHITE)
-pg.display.set_caption("Pathfinding")
+Surface = pg.display.set_mode((Window_height, Window_width))
+Surface.fill(WHITE)
+
+image = pg.display.get_surface()
+pg.display.set_caption("Path Of Exile")
 
 clock = pg.time.Clock()
 clock.tick(FPS)
@@ -38,12 +43,28 @@ def drawGrid():
         for y in range(0, Window_height, BlockSize):
             rect = pg.Rect(x, y, BlockSize, BlockSize)
             pg.draw.rect(Window, BLACK, rect, 1)
+            
+#De burning man#
+def ColorGrid (row, colom):
+    Coord_x = (row - 1) * BlockSize
+    Coord_y = (colom - 1) * BlockSize
+    print("coord X:", Coord_x, "Coord Y:", Coord_y) 
+
+    rect_green = pg.Rect(Coord_x, Coord_y, BlockSize, BlockSize)
+    image.fill(GREEN, rect_green)
+
+#The touch of innocence#
+def resetGrid():
+    Window.fill(WHITE)
+    Surface.fill(WHITE)
+    drawGrid()
+    pg.display.update()
 
 # run loop #
 run=True
 
 while run:
-	drawGrid()
+    drawGrid()
     pg.display.update()
 
     for event in pg.event.get():
@@ -53,16 +74,25 @@ while run:
             sys.exit()
             pg.display.update()
 
+        if event.type == pg.K_r:
+            resetGrid()
+
         elif event.type == pg.MOUSEBUTTONDOWN:
             pos = pg.mouse.get_pos()
+            row = (pos[0] // BlockSize) + 1
+            colom = (pos[1] // BlockSize) + 1
 
-            print("Click ", pos, "Grid coordinates: ", row, column)
-
+            ColorGrid(row, colom)
+            print("Click ", pos, "Grid coordinates: ", row, colom)
+        
     keys = pg.key.get_pressed()
     if keys[K_ESCAPE]:
         run=False
         pg.quit()
         sys.exit()
         pg.display.update()
+
+    if keys[K_r]:
+        resetGrid()
 
     pg.display.update()
