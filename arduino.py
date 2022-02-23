a4980a63a96e7aeaac3649205d252ff24d48f222
BentDeClerck <bent.declerck@outlook.com>
2022-02-23 16:03:18 +0100

    Serial communication fix

--- arduino.py
+++ arduino.py
@@ -1,12 +1,17 @@
 import serial 
+import time
 
-arduino = serial.Serial(port='COM6', baudrate=115200, timeout=.1)
+arduino = serial.Serial(port='COM6', baudrate=115200, timeout=1)
 
 def write(values):
     
-    arduino.write(b'values')
-    print(b'values')
+    arduino.write((values+"\n").encode("utf-8"))
+    time.sleep(0)
+    print(arduino.readline().decode('utf-8'))
     
-
-write("255,0,1,0,1,255")
-print(arduino.readline()) 
+while True:
+    time.sleep(1)
+    write("255,0,1,0,1,255")
+    time.sleep(1)
+    write("255,1,0,1,0,255")
+    
