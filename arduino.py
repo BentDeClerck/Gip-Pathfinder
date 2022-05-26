fe5862a37472c5c5e307545f67e4dbd28d7cc012
BentDeClerck <bent.declerck@outlook.com>
2022-05-26 16:10:19 +0200

    comments erbij geplaatst

--- arduino.py
+++ arduino.py
@@ -3,20 +3,16 @@ import time
 
 try:
     arduino = serial.Serial(port='COM3', baudrate=115200, timeout=0.5)
-    time.sleep(2)
+    time.sleep(2) # Even wachten voor goede connectie
 except Exception:
         print("geen connectie met arduino gevonden")
         
-
 def write(values):
-    
-    
-    arduino.write((values+"\n").encode("utf-8"))
+    arduino.write((values+"\n").encode("utf-8")) # Write array naar arduino 
     print(arduino.readline().decode('utf-8'))
     
 
 
 
-
 write("33")
-# 1 = rechtdoor; 2 = naarachter; 3 = draaien rechts
+# 1 = rechtdoor; 2 = naarachter; 3 = draaien rechts 
